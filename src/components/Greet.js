e057b6e28735ea6e24b1893971db2da7c3a62100
bahalla <bahalla.taoufiq@gmail.com>
2020-03-16 15:52:32 +0100

    Destructuring props and state

--- src/components/Greet.js
+++ src/components/Greet.js
@@ -1,9 +1,10 @@
 import React from "react";
 
 const Greet = (props) => {
+    const { name } = props;
     return (
         <div>
-            <h1> Hello { props.name }</h1>
+            <h1> Hello { name }</h1>
             { props.children }
         </div>
     );
